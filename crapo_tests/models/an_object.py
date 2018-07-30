d13b043ebc578f0a1b740fc78da2b5e43d1460b1
Julien <julien.carette@openflexo.org>
2018-07-30 18:51:37 +0200

    [FIX] missing security rules

--- crapo_tests/models/an_object.py
+++ crapo_tests/models/an_object.py
@@ -2,6 +2,7 @@
 # ©2018 Article 714
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
+from odoo import models
 
 class AnObject(models.Model):
     _name = 'crapo.business.object'
