a7eebfe8f4e4b4cba825848578d7cb6f1291b8f0
Julien <julien.carette@openflexo.org>
2018-07-30 18:43:00 +0200

    [FIX] missing import of model classes

--- base_crapo_workflow/models/__init__.py
+++ base_crapo_workflow/models/__init__.py
@@ -6,3 +6,11 @@ Created on 16 july 2018
 @copyright: ©2018 Article 714
 @license: AGPL v3
 '''
+
+from . import activity
+from . import business_object
+from . import crapo_config
+from . import state_automata
+from . import state
+from . import transition
+from . import workflow
