4e3967e38637702bc6d499e42727b4d70f338150
Damien Couppé <dcouppe.2801@apside-groupe.com>
2019-08-30 14:48:32 +0200

    [precondition_message] - [IMP] Replace condition char by a one2mnay to object condition

--- base_crapo_workflow/models/condition.py
+++ base_crapo_workflow/models/condition.py
@@ -1,31 +1,27 @@
 # ©2018 Article 714
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import fields, models, _, api, exceptions
+from odoo import fields, models
+
 
 class Condition(models.Model):
     """
     Condition definition
     """
+
     _name = "crapo.condition"
     _description = "Condition description"
 
-    name = fields.Char(
-        string="Name",
-        required=True
-    )
-    description = fields.Text(
-        string="Condition description",
-        required=True
-    )
-    condition = fields.Char(
-        string="Condition eval",
-        required=True
-    )
+    name = fields.Char(required=True)
+    description = fields.Text(required=True)
+    condition = fields.Char(required=True)
+
+    is_precondition = fields.Boolean(required=True, default=True)
+    is_postcondition = fields.Boolean(required=True, default=False)
 
     transition_id = fields.Many2one(
         string="Transition",
         comodel_name="crapo.transition",
         ondelete="cascade",
-        required=True
+        required=True,
     )
