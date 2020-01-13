492935d5b9c6d2c95296ad0b1438e3e3a7c06ec0
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-01-13 14:32:12 +0100

    [IMP] split carpo automaton and workflow

--- crapo_workflow/__manifest__.py
+++ crapo_workflow/__manifest__.py
@@ -3,7 +3,7 @@
 # @license: AGPL v3
 
 {
-    "name": u"Crapo: Automaton",
+    "name": u"Crapo: Workflow",
     "version": u"12.0.2.0.0",
     "category": u"Crapo Workflows",
     "author": u"Article714",
@@ -11,15 +11,8 @@
     "website": u"https://www.article714.org",
     "summary": u""" Fexible tool to manage automata & workflows
      for your odoo objects""",
-    "depends": ["queue_job", "crapo_base"],
+    "depends": ["queue_job", "connector", "crapo_base"],
     "data": [
-        "security/crapo_security.xml",
-        "security/crapo_action.xml",
-        "security/crapo_business_object.xml",
-        "security/automaton.xml",
-        "security/automaton_transition.xml",
-        "security/condition.xml",
-        "security/state.xml",
         "security/workflow.xml",
         "security/workflow_activity.xml",
         "security/workflow_transition.xml",
@@ -27,12 +20,7 @@
         "security/workflow_context_entry.xml",
         "actions/window_actions.xml",
         "views/crapo_menus.xml",
-        "views/crapo_state_views.xml",
-        "views/crapo_transition_views.xml",
-        "views/crapo_action_views.xml",
         "views/crapo_workflow_activity_views.xml",
-        "views/crapo_automaton_views.xml",
-        "views/crapo_condition_views.xml",
         "views/crapo_workflow_views.xml",
     ],
     "installable": True,
