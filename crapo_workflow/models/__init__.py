6210063a3631e652b651187ba02d3b1881fc628e
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-01-13 14:32:12 +0100

    [IMP] split carpo automaton and workflow

---
+++ crapo_workflow/models/__init__.py
@@ -0,0 +1,21 @@
+"""
+Created on 16 july 2018
+
+@author: C. Guychard
+@copyright: ©2018-2019 Article 714
+@license: AGPL v3
+"""
+
+
+from . import automaton
+from . import automaton_transition
+from . import business_object
+from . import condition
+from . import crapo_action
+from . import crapo_config
+from . import state
+from . import workflow
+from . import workflow_activity
+from . import workflow_transition
+from . import workflow_joiner
+from . import workflow_context
