cbbaece70822236ca6bb8ef8acf7b3ac20f1054c
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-01-15 09:22:55 +0100

    [ADD] Joiner event logical condition and some mechanics

--- crapo_workflow/components/workflow_listener.py
+++ crapo_workflow/components/workflow_listener.py
@@ -16,72 +16,79 @@ class WorkflowListener(Component):
     _name = "crapo.workflow.listener"
     _inherit = "base.event.listener"
 
-    # def on_transition(self, record, from_state, to_state):
-    #     logging.info(
-    #         "OOOOOOOO on_transition OOOOOOOO !!!!! %s, %s, %s, %s",
-    #         self,
-    #         record,
-    #         from_state,
-    #         to_state._module,
-    #     )
-    #     self.env["crapo.workflow.joiner.event"].with_context(
-    #         {"notify_joiner_event": True}
-    #     ).with_delay().notify(
-    #         "transition",
-    #         {
-    #             "record": record,
-    #             "from_state": from_state,
-    #             "to_state": to_state,
-    #         },
-    #     )
+    def on_transition(self, record, from_state, to_state):
+        logging.info(
+            "OOOOOOOO on_transition OOOOOOOO !!!!! %s, %s, %s, %s",
+            self,
+            record,
+            from_state,
+            to_state._module,
+        )
+        self.env["crapo.workflow.joiner.event"].with_context(
+            {"notify_joiner_event": True}
+        ).with_delay().notify(
+            "transition",
+            {
+                "record": record,
+                "from_state": from_state,
+                "to_state": to_state,
+            },
+        )
 
-    # @skip_if(
-    #     lambda self, record, fields: self.env.context.get(
-    #         "notify_joiner_event"
-    #     )
-    #     or record._module in ("queue", "connector")
-    # )
-    # def on_record_create(self, record, fields):
-    #     logging.info(
-    #         "OOOOOOOO on_record_create OOOOOOOO !!!!! %s, %s, %s",
-    #         self,
-    #         record._module,
-    #         fields,
-    #     )
-    #     self.env["crapo.workflow.joiner.event"].with_context(
-    #         {"notify_joiner_event": True}
-    #     ).with_delay().notify(
-    #         "record_create", {"record": record, "fields": fields}
-    #     )
+    def on_activity_ended(self, record):
+        logging.info(
+            "OOOOOOOO on_activity_ended OOOOOOOO !!!!! %s, %s", self, record
+        )
+        self.env["crapo.workflow.joiner.event"].with_context(
+            {"notify_joiner_event": True}
+        ).with_delay().notify("activity_ended", {"record": record})
 
-    # @skip_if(
-    #     lambda self, record, fields: self.env.context.get(
-    #         "notify_joiner_event"
-    #     )
-    #     or record._module in ("queue", "connector")
-    # )
-    # def on_record_write(self, record, fields):
-    #     logging.info(
-    #         "OOOOOOOO on_record_write OOOOOOOO !!!!! %s, %s, %s",
-    #         self,
-    #         record._module,
-    #         fields,
-    #     )
-    #     self.env["crapo.workflow.joiner.event"].with_context(
-    #         {"notify_joiner_event": True}
-    #     ).with_delay().notify(
-    #         "record_write", {"record": record, "fields": fields}
-    #     )
+    @skip_if(
+        lambda self, record, fields: self.env.context.get(
+            "notify_joiner_event"
+        )
+        or record._module in ("queue", "connector")
+    )
+    def on_record_create(self, record, fields):
+        logging.info(
+            "OOOOOOOO on_record_create OOOOOOOO !!!!! %s, %s, %s",
+            self,
+            record._module,
+            fields,
+        )
+        self.env["crapo.workflow.joiner.event"].with_context(
+            {"notify_joiner_event": True}
+        ).with_delay().notify(
+            "record_create", {"record": record, "fields": fields}
+        )
 
-    # @skip_if(
-    #     lambda self, record: self.env.context.get("notify_joiner_event")
-    #     or record._module in ("queue", "connector")
-    # )
-    # def on_record_unlink(self, record):
-    #     logging.info(
-    #         "OOOOOOOO on_record_unlink OOOOOOOO !!!!! %s, %s", self, record
-    #     )
-    #     self.env["crapo.workflow.joiner.event"].with_context(
-    #         {"notify_joiner_event": True}
-    #     ).with_delay().notify("record_unlink", {"record": record})
+    @skip_if(
+        lambda self, record, fields: self.env.context.get(
+            "notify_joiner_event"
+        )
+        or record._module in ("queue", "connector")
+    )
+    def on_record_write(self, record, fields):
+        logging.info(
+            "OOOOOOOO on_record_write OOOOOOOO !!!!! %s, %s, %s",
+            self,
+            record._module,
+            fields,
+        )
+        self.env["crapo.workflow.joiner.event"].with_context(
+            {"notify_joiner_event": True}
+        ).with_delay().notify(
+            "record_write", {"record": record, "fields": fields}
+        )
 
+    @skip_if(
+        lambda self, record: self.env.context.get("notify_joiner_event")
+        or record._module in ("queue", "connector")
+    )
+    def on_record_unlink(self, record):
+        logging.info(
+            "OOOOOOOO on_record_unlink OOOOOOOO !!!!! %s, %s", self, record
+        )
+        self.env["crapo.workflow.joiner.event"].with_context(
+            {"notify_joiner_event": True}
+        ).with_delay().notify("record_unlink", {"record": record})
--- crapo_workflow/models/workflow_joiner.py
+++ crapo_workflow/models/workflow_joiner.py
@@ -3,7 +3,8 @@
 
 import logging
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+from odoo.exceptions import ValidationError
 from odoo.addons.queue_job.job import job
 from odoo.tools.safe_eval import safe_eval
 
@@ -21,15 +22,67 @@ class WorkflowJoiner(models.Model):
 
     event_ids = fields.One2many("crapo.workflow.joiner.event", "joiner_id")
 
+    event_logical_condition = fields.Char()
+
+    @job
     def check_and_run(self):
-        if all(self.event_ids.mapped("done")):
-            for activity_id in self.to_activity_ids:
-                activity_id.with_delay().run_async()
+        for rec in self:
+            if rec.event_logical_condition:
+                context = {}
+                for event in rec.event_ids:
+                    context[event.name] = event.done
+                run = safe_eval(rec.event_logical_condition, context)
+            else:
+                run = all(rec.event_ids.mapped("done"))
+
+            if run:
+                for activity_id in rec.to_activity_ids:
+                    activity_id.with_delay().run()
+
+    def check_event_logical_condition(self):
+        for rec in self:
+            if rec.event_logical_condition:
+                context = {}
+                for event in rec.event_ids:
+                    event_name = event.name
+                    context[event_name] = event.done
+                    if not event_name in rec.event_logical_condition:
+                        raise ValidationError(
+                            _("Event ({}) is not used in {}").format(
+                                event_name,
+                                self._fields["event_logical_condition"].string,
+                            )
+                        )
+                safe_eval(rec.event_logical_condition, context)
+
+    @api.model
+    def create(self, values):
+        rec = super(WorkflowJoiner, self).create(values)
+        if values.get("event_logical_condition"):
+            rec.check_event_logical_condition()
+        return rec
+
+    @api.multi
+    def write(self, values):
+        res = super(WorkflowJoiner, self).write(values)
+        if values.get("event_logical_condition"):
+            self.check_event_logical_condition()
+        return res
 
 
 class WorkflowJoinerEvent(models.Model):
 
     _name = "crapo.workflow.joiner.event"
+
+    _sql_constraints = [
+        (
+            "unique_name_per_joiner_id",
+            "unique(name, joiner_id)",
+            "Joiner event can't have the same name in the same joiner",
+        )
+    ]
+
+    name = fields.Char()
 
     joiner_id = fields.Many2one("crapo.workflow.joiner")
 
@@ -49,6 +102,7 @@ class WorkflowJoinerEvent(models.Model):
             ("record_create", "record_create"),
             ("record_write", "record_write"),
             ("record_unlink", "record_unlink"),
+            ("activity_ended", "activity_ended"),
         ]
     )
 
@@ -60,6 +114,7 @@ class WorkflowJoinerEvent(models.Model):
     def notify(self, event_type, values):
 
         domain = [("event_type", "=", event_type), ("done", "=", False)]
+        context = {"env": self.env, "values": values}
         if event_type == "transition":
             from_state = values["from_state"]
             to_state = values["to_state"]
@@ -71,40 +126,45 @@ class WorkflowJoinerEvent(models.Model):
 
         else:
             record = values["record"]
-            logging.info(
-                "PPPPPPPPPPPPPPPPP %s",
-                self.env.ref(
-                    "{}.model_{}".format(
-                        record._module, record._name.replace(".", "_")
-                    )
-                ),
-            )
+
             domain.append(
-                (
-                    "model_id",
-                    "=",
-                    self.env.ref(
-                        "{}.model_{}".format(
-                            record._module, record._name.replace(".", "_")
-                        )
-                    ).id,
-                )
+                ("model_id", "=", self.env["ir.model"]._get_id(record._name),)
             )
 
-            if event_type in ("record_write", "record_unlink"):
+            if event_type in (
+                "record_write",
+                "record_unlink",
+                "activity_ended",
+            ):
                 domain.append(("record_id", "=", record.id))
 
         logging.info("SAERCH DOMAIN : %s", domain)
-        self.search(domain).filtered(
-            lambda rec: not rec.condition
-            or safe_eval(rec.condition, {"object": rec, "env": self.env},)
-        ).write({"done": True})
+        records = self.search(domain).filtered(
+            lambda rec: not rec.condition or safe_eval(rec.condition, context,)
+        )
+        if records:
+            records.write({"done": True})
+
+    # ================================
+    # Write / Create
+    # ================================
+
+    @api.model
+    def create(self, values):
+
+        rec = super(WorkflowJoinerEvent, self).create(values)
+
+        if not rec.name:
+            rec.name = "_".join((rec.event_type, str(rec.id)))
+
+        return rec
 
     @api.multi
     def write(self, values):
+
         res = super(WorkflowJoinerEvent, self).write(values)
 
         if values.get("done"):
-            self.mapped("joiner_id").check_and_run()
+            self.mapped("joiner_id").with_delay().check_and_run()
 
         return res
