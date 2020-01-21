3e3342f12001d381e903af96e47da3d5f9e77771
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-01-21 16:18:44 +0100

    [IMP] Split connector workflow

---
+++ crapo_workflow_connector/__manifest__.py
@@ -0,0 +1,19 @@
+# @author: C. Guychard
+# @copyright: ©2018-2019 Article 714
+# @license: AGPL v3
+
+{
+    "name": u"Crapo: Workflow",
+    "version": u"12.0.2.0.0",
+    "category": u"Crapo Workflows",
+    "author": u"Article714",
+    "license": u"AGPL-3",
+    "website": u"https://www.article714.org",
+    "summary": u""" Fexible tool to manage automata & workflows
+     for your odoo objects""",
+    "depends": ["crapo_workflow", "connector"],
+    "data": [],
+    "installable": True,
+    "images": [],
+    "application": True,
+}
---
+++ crapo_workflow_connector/tests/installation_test.py
@@ -0,0 +1,27 @@
+# ©2019 Article 714
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+
+from odoo.tests import common
+
+
+class IntegrationTestSuite(common.TransactionCase):
+    """
+    Oversimple test suite to test if module is installed
+    """
+
+    def test_module_is_installed(self):
+        """
+        Testing that current module is installed
+        """
+        module_model = self.env["ir.module.module"]
+        self.assertIsNotNone(module_model)
+
+        found_modules = module_model.search(
+            [
+                ("name", "=", "crapo_workflow_connector"),
+                ("state", "=", "installed"),
+            ]
+        )
+
+        self.assertEqual(len(found_modules), 1, "Module not installed")
