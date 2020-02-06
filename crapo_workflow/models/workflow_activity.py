e8a3b2c71bf8d916d0275f34eb0a9aa1ed90d6fc
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-02-06 13:41:06 +0100

    [IMP] Change worflow activity inherit from ir.action.server to inherits

--- crapo_workflow/models/workflow_activity.py
+++ crapo_workflow/models/workflow_activity.py
@@ -6,18 +6,32 @@ from odoo import models, fields, api
 from odoo.addons.queue_job.job import job
 
 
+class IrActionsServer(models.Model):
+    _inherit = "ir.actions.server"
+
+    usage = fields.Selection(
+        selection_add=[("crapo_workflow_activity", "Crapo workflow activity")]
+    )
+
+
 class WorkflowActivity(models.Model):
     """
     An activity step in a Workflow, i.e. a step where something must be done
     """
 
     _name = "crapo.workflow.activity"
-    _inherit = "ir.actions.server"
+    _inherits = {"ir.actions.server": "action_server_id"}
     _description = (
         "Workflow activity: a specialization of server actions for Crapo"
     )
 
+    action_server_id = fields.Many2one(
+        "ir.actions.server", required=True, ondelete="restrict"
+    )
+
     workflow_id = fields.Many2one("crapo.workflow", ondelete="cascade")
+
+    record_context_key = fields.Char()
 
     @job
     @api.multi
@@ -29,8 +43,23 @@ class WorkflowActivity(models.Model):
         context = {"wf": wf_context_id, "logging": logging}
 
         res = False
-        for rec in self.with_context(**context):
-            res = super(WorkflowActivity, rec).run()
-            rec._event("on_activity_ended").notify(rec)
+        for rec in self:
+            if rec.record_context_key:
+                active_record = wf_context_id.get_context_entry(
+                    rec.record_context_key
+                )
+                context["active_id"] = active_record.id
+                context["active_model"] = active_record._name
 
+            res = rec.with_context(**context).action_server_id.run()
+            rec.wf_event("on_activity_ended")
         return res
+
+    # ==============================
+    # Write/Create
+    # ==============================
+
+    @api.model
+    def create(self, values):
+        values["usage"] = "crapo_workflow_activity"
+        return super(WorkflowActivity, self).create(values)
