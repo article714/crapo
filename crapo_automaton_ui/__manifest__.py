bb2f3e496fdf425f276cee3925938278b356e343
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-02-06 14:53:38 +0100

    [IMP] Split UI of crapo automaton

--- crapo_automaton_ui/__manifest__.py
+++ crapo_automaton_ui/__manifest__.py
@@ -3,7 +3,7 @@
 # @license: AGPL v3
 
 {
-    "name": u"Crapo: Automaton",
+    "name": u"Crapo: Automaton UI",
     "version": u"12.0.2.0.0",
     "category": u"Crapo Workflows",
     "author": u"Article714",
@@ -11,14 +11,15 @@
     "website": u"https://www.article714.org",
     "summary": u""" Fexible tool to manage automata & workflows
      for your odoo objects""",
-    "depends": ["queue_job", "crapo_base"],
+    "depends": ["crapo_automaton"],
     "data": [
-        "security/automaton_action.xml",
-        "security/crapo_business_object.xml",
-        "security/automaton.xml",
-        "security/automaton_transition.xml",
-        "security/condition.xml",
-        "security/state.xml",
+        "actions/window_actions.xml",
+        "views/crapo_menus.xml",
+        "views/crapo_state_views.xml",
+        "views/crapo_transition_views.xml",
+        "views/crapo_automaton_action_views.xml",
+        "views/crapo_automaton_views.xml",
+        "views/crapo_condition_views.xml",
     ],
     "installable": True,
     "images": [],
