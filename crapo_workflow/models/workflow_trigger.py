3d01a4e364f7d11e842f08787f62f985db719376
Damien Couppé <dcouppe.2801@apside-groupe.com>
2020-02-07 13:20:41 +0100

    [IMP] Add docstring and comments + some refactoring

--- crapo_workflow/models/workflow_trigger.py
+++ crapo_workflow/models/workflow_trigger.py
@@ -1,65 +1,98 @@
 # ©2018-2019 Article 714
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-import logging
+import re
 
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
-from odoo.addons.queue_job.job import job
 from odoo.tools.safe_eval import safe_eval
+
+from odoo.addons.queue_job.job import job
 
 
 class WorkflowTrigger(models.Model):
     """
-        A trigger step in a Workflow
+        A trigger is the logical brick between activities. It will trigger all of it activities
+        when event_logical_condition is True.
+
+        - Initial trigger:  this is the first trigger of the workflow it will
+          initiate a crapo.workflow.context when triggered
+
+        - End trigger: this is the last trigger of the workflow it will distroy
+          the crapo.workflow.context that triggered it
     """
 
     _name = "crapo.workflow.trigger"
 
-    name = fields.Char()
+    name = fields.Char(required=True)
 
     workflow_id = fields.Many2one("crapo.workflow")
 
+    trigger_type = fields.Selection(
+        [
+            ("init", "Initiale trigger"),
+            ("end", "End trigger"),
+            ("joiner", "Joiner trigger between activities"),
+        ],
+        required=True,
+        default="joiner",
+    )
+
     to_activity_ids = fields.Many2many(
-        "crapo.workflow.activity", "crapo_workflow_tj_trigger_activity"
+        "crapo.workflow.activity",
+        "crapo_workflow_tj_trigger_activity",
+        domain="[('workflow_id', '=', workflow_id)]",
     )
     from_activity_ids = fields.Many2many(
-        "crapo.workflow.activity", "crapo_workflow_tj_activity_trigger"
+        "crapo.workflow.activity",
+        "crapo_workflow_tj_activity_trigger",
+        domain="[('workflow_id', '=', workflow_id)]",
     )
 
     event_ids = fields.One2many("crapo.workflow.event", "trigger_id")
 
-    event_logical_condition = fields.Char()
-
-    init_trigger = fields.Boolean(default=False, required=True)
-
-    end_trigger = fields.Boolean(default=False, required=True)
+    event_logical_condition = fields.Char(
+        help="If empty, all event must be done to trigger the trigger"
+    )
 
     init_record_key = fields.Char()
 
     @job
     def check_and_run(self, wf_context_id):
+        """
+            Evaluate event_logical_condition in the context passer in parameter
+            to see if trigger is triggered. If so run activities or destroy wf context
+            in case of end trigger type.
+        """
         for rec in self:
             context_event_ids = wf_context_id.context_event_ids.filtered(
                 lambda context_event: context_event.trigger_id == rec
             )
+            # Evaluate event_logical_condition if there is one
             if rec.event_logical_condition:
                 context = {"wf": wf_context_id}
                 for context_event in context_event_ids:
                     context[context_event.event_id.name] = context_event.done
 
                 run = safe_eval(rec.event_logical_condition, context)
+            # Else we considerate that all event must be done
             else:
                 run = all(context_event_ids.mapped("done"))
 
             if run:
-                if rec.end_trigger:
+                # Destroy context
+                if rec.trigger_type == "end":
                     wf_context_id.unlink()
+
+                # Run activities
                 else:
                     for activity_id in rec.to_activity_ids:
-                        rec.start_activity(activity_id, wf_context_id)
+                        rec.run_activity(activity_id, wf_context_id)
 
-    def start_activity(self, activity_id, wf_context_id):
+    def run_activity(self, activity_id, wf_context_id):
+        """
+            Run activity passed in parameter with the context passed in parameter
+        """
         self.ensure_one()
 
         for rec in wf_context_id.context_event_ids.filtered(
@@ -81,6 +114,10 @@ class WorkflowTrigger(models.Model):
 
     @api.onchange("from_activity_ids")
     def activity_ended_event_consistency(self):
+        """
+            Automaticaly add or remove activity_ended event based on
+            from activity_ids field
+        """
         self.ensure_one()
         values = []
 
@@ -117,19 +154,27 @@ class WorkflowTrigger(models.Model):
             self.update({"event_ids": values})
 
     def check_event_logical_condition(self):
+        """
+            Validate that all event are used in the condition
+        """
         for rec in self:
             if rec.event_logical_condition:
                 context = {}
+                # Check if all event are found in event_logical_condition
                 for event in rec.event_ids:
                     event_name = event.name
                     context[event_name] = False
-                    if not event_name in rec.event_logical_condition:
+                    # Regexp to extract all word (and so all variable) from
+                    # event_logical_condition
+                    variables = re.findall(r"\w+", rec.event_logical_condition)
+                    if not event_name in variables:
                         raise ValidationError(
                             _("Event ({}) is not used in {}").format(
                                 event_name,
                                 self._fields["event_logical_condition"].string,
                             )
                         )
+                # Check syntax
                 safe_eval(rec.event_logical_condition, context)
 
     def check_init_trigger(self):
@@ -137,10 +182,14 @@ class WorkflowTrigger(models.Model):
             Test if an init trigger has at most 1 trigger event
         """
         for rec in self:
-            if rec.init_trigger and len(rec.event_ids) > 1:
+            if rec.trigger_type == "init" and len(rec.event_ids) > 1:
                 raise ValidationError(
                     _("Init trigger can have only one trigger event")
                 )
+
+    # ==========================
+    # Write / Create
+    # ==========================
 
     @api.model
     def create(self, values):
