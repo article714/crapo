5dc9303b72b4da67c4a3fd87298811b235555fc6
Damien Couppé <5243814+Kontrolix@users.noreply.github.com>
2020-04-01 09:01:07 +0200

    [IMP] Add missing import

--- crapo_automaton/models/automaton_mixin.py
+++ crapo_automaton/models/automaton_mixin.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from odoo.tools.safe_eval import safe_eval
+from odoo import SUPERUSER_ID
 
 from ..mixins.crapo_readonly_view_mixin import ReadonlyViewMixin
 
